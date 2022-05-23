0205cf4112b24e2021db81488aa1677a30a112a9
FriedWagyu <60728947+FriedWagyu@users.noreply.github.com>
2022-05-23 21:47:49 +0800

    op

--- Play.sh
+++ Play.sh
@@ -4,11 +4,4 @@ cd /home/pi/Desktop/random-voxel/
 
 gh repo sync
 
-cd /home/pi/Desktop/random-voxel/op_mkv/
-
-while [ 1 -gt 0 ]
-do
-FILE=$(ls | shuf -n 1)
-echo $FILE
-nvlc $FILE
-done
+nvlc --no-osd --loop --random "op_mkv" --key-toggle-fullscreen
